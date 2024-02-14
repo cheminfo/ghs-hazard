98851c88ac766cd1dc5c132a61f4d345019d0a60
Luc Patiny <luc@patiny.com>
2024-02-14 16:18:00 +0100

    feat: Expose TS interface

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,27 @@
 import { ghs } from './data/ghscode_10';
 
-export function getHStatements() {
+export interface GHS {
+  ghsCode: string;
+  ghsPictogramText: string;
+  ghsPictogramSVG: string;
+}
+
+export interface HStatement {
+  hCode: string;
+  hStatement: string;
+}
+
+export interface PStatement {
+  pCode: string;
+  pStatement: string;
+}
+
+export function getHStatements(): HStatement[] {
   return ghs.hStatements;
 }
 
 export function getHStatementsObject() {
-  const result = {};
+  const result: Record<string, HStatement> = {};
   ghs.hStatements.forEach((h) => {
     if (result[h.hCode]) {
       throw new Error(`This should never happen, duplice hCode: ${h.hCode}`);
@@ -15,12 +31,12 @@ export function getHStatementsObject() {
   return result;
 }
 
-export function getPStatements() {
+export function getPStatements(): PStatement[] {
   return ghs.pStatements;
 }
 
 export function getPStatementsObject() {
-  const result = {};
+  const result: Record<string, PStatement> = {};
   ghs.pStatements.forEach((p) => {
     if (result[p.pCode]) {
       throw new Error(`This should never happen, duplice pCode: ${p.pCode}`);
@@ -30,12 +46,12 @@ export function getPStatementsObject() {
   return result;
 }
 
-export function getGHS() {
+export function getGHS(): GHS[] {
   return ghs.ghsPictograms;
 }
 
 export function getGHSObject() {
-  const result = {};
+  const result: Record<string, GHS> = {};
   ghs.ghsPictograms.forEach((g) => {
     if (result[g.ghsCode]) {
       throw new Error(`This should never happen, duplice code: ${g.ghsCode}`);
